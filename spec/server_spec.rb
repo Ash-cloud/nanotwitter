162415cc4a0ede298f8c26c03182f53f08ed600e
jinfenglin <ljfnwpusife@gmail.com>
2015-03-24 14:27:58 -0400

    code ship

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,12 +1,9 @@
-ENV['SINATRA_ENV'] = 'test'
 require_relative '../app.rb'
 
 describe "server" do 
     before(:all) do
         #setting up the environment for testing
-        env_index = ARGV.index("-e")
-        env_arg = ARGV[env_index + 1] if env_index
-        env = env_arg || ENV["SINATRA_ENV"] || "development"
+        env = :test
         databases = YAML.load_file("config/database.yml")
         puts "########"+databases[env].to_s
         ActiveRecord::Base.establish_connection(databases[env])
